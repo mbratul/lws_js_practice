5d8aeebfa8088f2ee580e3c50bf861be9d3c23c7
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-05-22 07:48:48 +0600

    describe array constant in this file

---
+++ array_constant.js
@@ -0,0 +1,5 @@
+/* 
+    Array Constant Describe here
+    youtube url: https://www.youtube.com/watch?v=MV15SLCqcmY&list=PLHiZ4m8vCp9OkrURufHpGUUTBjJhO9Ghy&index=34&ab_channel=LearnwithSumit-LWS-Bangladesh
+    w3 url : https://www.w3schools.com/js/js_array_const.asp
+ */
