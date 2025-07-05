76691ac20539c88abe4b9bd100420f840b251ad4
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-05 13:54:30 +0600

    arrow function describe here

---
+++ arrow_function.js
@@ -0,0 +1,18 @@
+/* 
+Arrow Function in JS
+url: https://youtu.be/UwrmEUCaAIY?si=jRnjCAT1OvmE2hkS
+ */
+
+function number() {
+  return 10;
+}
+console.log(number());
+
+/* 
+below is the arrow function body 
+() => {} 
+ */
+let number1 = () => {
+  return 10;
+};
+console.log(number1());
