a4bba43b8137f7dbd9737498cdebefbcce8bd522
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-06 05:58:21 +0600

    update arrow function

--- arrow_function.js
+++ arrow_function.js
@@ -13,6 +13,28 @@ below is the arrow function body
 () => {} 
  */
 let number1 = () => {
-  return 10;
+  return 11;
 };
 console.log(number1());
+
+let number2 = () => 12; //single line arrow function statement
+console.log(number2());
+
+let number3 = () => console.log(13);
+number3();
+
+let number4 = (a, b) => {
+  return a + b;
+};
+console.log(number4(10, 20));
+
+let javascript = {
+  name: "JavaScript",
+  libraries: ["React", "Angular", "Vue"],
+  printLibraries: function () {
+    this.libraries.forEach((a) => {
+      console.log(`${this.name} loves ${a}`);
+    });
+  },
+};
+javascript.printLibraries();
